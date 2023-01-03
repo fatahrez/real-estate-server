152305dd8f5a287a1f95209ee6b399993e3c092c
fatahrez <fertahrez@gmail.com>
2023-01-03 19:18:54 +0300

    feature: add documentation with swagger and redoc chore: add .env.sample file

--- real_estate/config/urls.py
+++ real_estate/config/urls.py
@@ -18,7 +18,27 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Real Estate API",
+        default_version="v1",
+        description="Real Estate API for iOS and android app",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="fertahrez@gmail.com"),
+        license=openapi.License(name="Apache License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
+
 urlpatterns = [
     path("realestateadmin/", admin.site.urls),
     path("api/", include('real_estate.apps.users.urls')),
+
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name="schema-redoc-ui")
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
