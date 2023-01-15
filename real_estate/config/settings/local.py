4afb0eeacda2bf49a3fbea2687de19b0d856f4dd
fatahrez <fertahrez@gmail.com>
2023-01-16 00:34:38 +0300

    Bug: Nginx/db

--- real_estate/config/settings/local.py
+++ real_estate/config/settings/local.py
@@ -6,9 +6,9 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
         "HOST": env("DB_HOST"),
         "PORT": env("DB_PORT"),
     }
