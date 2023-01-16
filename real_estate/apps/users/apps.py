6d3e45dfbd0da505d67cd4d5ef57e0a42d865d37
fatahrez <fertahrez@gmail.com>
2023-01-16 21:02:21 +0300

    Chore: Make property accessible without authentication

--- real_estate/apps/users/apps.py
+++ real_estate/apps/users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "real_estate.apps.users"
+
+    def ready(self):
+        from real_estate.apps.users import models
