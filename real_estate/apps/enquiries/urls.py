64de3e9ee3f3633d7aeaebf474fd000011d0e82c
fatahrez <fertahrez@gmail.com>
2023-03-10 14:20:29 +0300

    add enquiry details endpoint

--- real_estate/apps/enquiries/urls.py
+++ real_estate/apps/enquiries/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("enquiry/all/", views.ListAllEnquiryAPIView.as_view(), name="all-enquries"),
+    path("enquiry/details/<int:id>/", views.EnquiryDetailView.as_view(), name="enquiry-details"),
     path("enquiry/create/", views.send_enquiry_email, name="send-enquiry"),
 ]
